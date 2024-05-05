c7f0f3c904779904882c5cad3c0c6a02762f3507
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-05-05 20:27:00 +0200

    feat(backend): create main directories

---
+++ backend/SocialMediaManagerApi/src/SocialMediaManager.Api/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMediaManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ConfigurationBuilder
+{
+    
+}
