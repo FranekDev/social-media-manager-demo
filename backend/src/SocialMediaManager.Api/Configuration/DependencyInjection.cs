3035a57025844ac912891d624e1f87f13465ddd9
Franek Wojtas <49079734+FranekDev@users.noreply.github.com>
2024-05-09 10:58:21 +0200

    refactor: change root directories for frontend and backend

---
+++ backend/src/SocialMediaManager.Api/Configuration/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialMediaManager.Api.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
